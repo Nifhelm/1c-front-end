83476e7b5f400ec7fcfb405f315914f9c198e9f8
Nifhelm <n@newair.ru>
2017-02-09 16:11:58 +0300

    12

--- seminars/1. Canvas/script.js
+++ seminars/1. Canvas/script.js
@@ -1,4 +1,21 @@
-var canvas = document.getElementById("sandbox")
-var context = canvas.getContext("2d")
+var canvas = document.getElementById("sandbox"),
+    context = canvas.getContext("2d"),
+    line = new Path2D(),
+    square, circle;
 
-context.fillRect(50, 25, 200, 250)
+square = new Path2D();
+square.moveTo(50, 50);
+square.lineTo(250, 50);
+square.lineTo(250, 250);
+square.LineTo(50, 250);
+square.lineTo(50, 50);
+
+context.stroke(square);
+12121
+circle = new Path2D();
+circle.arc(150, 150, 100, 0, 2 * Math.PI);
+
+context.fill(circle);
+
+line.moveTo(0, 0);
+line.lineTo(300, 300);
